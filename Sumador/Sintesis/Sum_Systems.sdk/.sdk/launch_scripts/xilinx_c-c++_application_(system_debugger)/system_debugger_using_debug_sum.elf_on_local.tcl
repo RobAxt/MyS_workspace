b6345b74db22ea331e159e5ad18f19a87f279361
AXT Roberto <ROA@tgn.com.ar>
2025-06-14 18:04:37 -0300

    intento de sumador de 8bits

---
+++ Sumador/Sintesis/Sum_Systems.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_sum.elf_on_local.tcl
@@ -0,0 +1,18 @@
+connect -url tcp:127.0.0.1:3121
+source C:/Xilinx/MyS_workspace/Sumador/Sintesis/Sum_Systems.sdk/design_1_wrapper_hw_platform_0/ps7_init.tcl
+targets -set -filter {jtag_cable_name =~ "Digilent Arty Z7 003017B7EDF5A" && level==0} -index 1
+fpga -file C:/Xilinx/MyS_workspace/Sumador/Sintesis/Sum_Systems.sdk/design_1_wrapper_hw_platform_0/design_1_wrapper.bit
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent Arty Z7 003017B7EDF5A"} -index 0
+loadhw -hw C:/Xilinx/MyS_workspace/Sumador/Sintesis/Sum_Systems.sdk/design_1_wrapper_hw_platform_0/system.hdf -mem-ranges [list {0x40000000 0xbfffffff}]
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent Arty Z7 003017B7EDF5A"} -index 0
+stop
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Arty Z7 003017B7EDF5A"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Arty Z7 003017B7EDF5A"} -index 0
+dow C:/Xilinx/MyS_workspace/Sumador/Sintesis/Sum_Systems.sdk/sum/Debug/sum.elf
+configparams force-mem-access 0
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Arty Z7 003017B7EDF5A"} -index 0
+con
